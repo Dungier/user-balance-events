740d3b7216ee11e951d1a36b2dfe15e841288507
Dungier <131267413+Dungier@users.noreply.github.com>
2024-09-20 20:40:44 +0300

    another microfix :)

--- src/users/services/user.service.ts
+++ src/users/services/user.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import UsersRepository from '../../shared/repository/users.repository';
 import HistoryRepository from '../../shared/repository/history.repository';
 import { CreateHistoryEventDto } from '../../shared/dto/create-history-event.dto';
+import { BalanceActionTypeEnum } from '../../shared/enums/balance-action-type.enum';
+import UsersEntity from '../../shared/entitis/users.entity';
 
 @Injectable()
 export default class UserService {
@@ -15,8 +17,23 @@ export default class UserService {
   }
 
   public async createBalanceEvent(dto: CreateHistoryEventDto) {
+    const isValid = await this.isValidOperation(dto);
+
+    if (!isValid) {
+      throw new BadRequestException('Balance cannot < 0');
+    }
     await this.historyRepository.createEvent(dto);
 
     return this.userRepository.getById(dto.user_id);
+  }
+
+  public async isValidOperation(dto: CreateHistoryEventDto) {
+    const user = await this.userRepository.getById(dto.user_id);
+
+    if (dto.action === BalanceActionTypeEnum.Debit) {
+      return user.balance - dto.amount >= 0;
+    }
+
+    return true;
   }
 }
